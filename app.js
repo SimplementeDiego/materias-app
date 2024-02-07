4169b97d3391396f90cc458d138f312c4f93ab18
SimplementeDiego <diegodellepianefurrer@gmail.com>
2024-02-07 18:00:07 -0300

    Agregue la posibilidad de salvar el curso y no exonerar

--- app.js
+++ app.js
@@ -9,48 +9,52 @@ class Materia {
         this.creditos = creditos;
         this.nombre = nombre;
         this.previas = previas;
+        this.curso = `${nombre}Curso`
+        this.estado = 0;
     }
 
 }
 
 const CalculoDIV = new Materia("CalculoDIV", 13, []);
-const CalculoDIVV = new Materia("CalculoDIVV", 13, [CalculoDIV]);
+const CalculoDIVV = new Materia("CalculoDIVV", 13, [CalculoDIV.curso]);
 const MD1 = new Materia("MD1", 9, []);
 const P1 = new Materia("P1", 10, []);
 const GAL1 = new Materia("GAL1", 9, [])
-const P2 = new Materia("P2", 12, [P1]);
+const P2 = new Materia("P2", 12, [P1.curso]);
 const Ec = new Materia("Ec", 7, [])
 const F1 = new Materia("F1", 10, [])
-const GAL2 = new Materia("GAL2", 9, [GAL1])
-const PYE = new Materia("PYE", 10, [CalculoDIVV, GAL1])
-const MD2 = new Materia("MD2", 9, [MD1])
-const LG = new Materia("LG", 12, [MD1])
-const MN = new Materia("MN", 8, [CalculoDIVV, P1, GAL2])
-const P4 = new Materia("P4", 15, [GAL1, CalculoDIV, MD1, P2])
-const IIO = new Materia("IIO", 10, [GAL2, PYE])
-const P3 = new Materia("P3", 15, [P2, PYE])
-const AC = new Materia("AC", 12, [CalculoDIV, LG, P2])
-const CV = new Materia("CV", 10, [CalculoDIVV, GAL1])
-const ED = new Materia("ED", 10, [GAL2,CalculoDIVV])
-const TL = new Materia("TL", 12, [P3, CalculoDIV, GAL1])
-const FVC = new Materia("FVC", 5, [CV])
-const SO = new Materia("SO", 12, [AC, GAL1])
+const GAL2 = new Materia("GAL2", 9, [GAL1.curso])
+const PYE = new Materia("PYE", 10, [CalculoDIVV.curso, GAL1, CalculoDIV])
+const MD2 = new Materia("MD2", 9, [MD1.curso, GAL1.curso]) 
+const LG = new Materia("LG", 12, [MD1.curso]) 
+const MN = new Materia("MN", 8, [CalculoDIVV, P1, GAL2, GAL1, CalculoDIV]) 
+const P4 = new Materia("P4", 15, [GAL1, CalculoDIV, MD1, P2]) 
+const IIO = new Materia("IIO", 10, [GAL2, PYE, CalculoDIVV, CalculoDIV, GAL1]) 
+const P3 = new Materia("P3", 15, [P2.curso, PYE.curso, P1, MD1]) 
+const AC = new Materia("AC", 12, [CalculoDIV, LG.curso, P2.curso, P1, MD1]) 
+const CV = new Materia("CV", 10, [CalculoDIVV.curso, GAL1, CalculoDIV]) 
+const ED = new Materia("ED", 10, [GAL2,CalculoDIVV.curso, GAL1, CalculoDIV]) 
+const TL = new Materia("TL", 12, [P3.curso, CalculoDIV, GAL1, LG, MD1]) 
+const FVC = new Materia("FVC", 5, [CV.curso, CalculoDIVV]) 
+const SO = new Materia("SO", 12, [AC.curso, GAL1, CalculoDIV, P2, MD1])
 const FO = new Materia("FO", 6, [IIO])
-const OCA = new Materia("OCA", 10, [IIO])
-const FBD = new Materia("FBD", 15, [LG, P3, MD2])
-const RC = new Materia("RC", 12, [P3, SO])
-const TP = new Materia("TP", 15, [P4])
-const IIS = new Materia("IIS", 10, [TP, FBD])
-const IPF = new Materia("IPF", 7, [TL, P2, LG])
-const PL = new Materia("PL", 10, [TL, P3, MD2, LG])
-const PIS = new Materia("PIS", 15, [IIS])
-const AA = new Materia("AA", 12, [LG, PYE, P3, FBD, P4, MD2])
-const AE = new Materia("AE", 10, [P4, PYE, IIO])
-const ICG = new Materia("ICG", 10, [P3, P4, GAL2])
-const CGA = new Materia("CGA", 12, [ICG])
-const CG = new Materia("CG", 10, [MD2, P3])
-const FSI = new Materia("FSI", 12, [SO, FBD, P3, LG, RC])
+const OCA = new Materia("OCA", 10, [IIO]) 
+const FBD = new Materia("FBD", 15, [LG, P3, MD2]) 
+const RC = new Materia("RC", 12, [P3, SO.curso, CalculoDIV, AC.curso])
+const TP = new Materia("TP", 15, [P4]) //Hay otra forma de poder cursar TP, P4.curso + P3 | Tengo que revisar como mostrarlo
+const IIS = new Materia("IIS", 10, [TP.curso, FBD.curso, P4.curso]) 
+const IPF = new Materia("IPF", 7, [TL, P2, LG, MD1]) 
+const PL = new Materia("PL", 10, [TL, P3, MD2, LG]) 
+const PIS = new Materia("PIS", 15, [IIS.curso, P4]) 
+const AA = new Materia("AA", 12, [LG, PYE, P3, FBD, P4, MD2]) 
+const AE = new Materia("AE", 10, [P4, PYE, IIO]) 
+const ICG = new Materia("ICG", 10, [P3, P4, GAL2.curso, GAL1]) 
+const CGA = new Materia("CGA", 12, [ICG.curso]) 
+const CG = new Materia("CG", 10, [MD2, P3, MD1]) 
+const FSI = new Materia("FSI", 12, [SO, FBD, P3, LG, RC]) //hasta aca revise
 const PG = new Materia("PG", 30, [P3, P4, TL, SO, IIO, IIS, AC, RC, TP, PIS, MN, FBD]) //falta agregar
+
+//Revisar todos otra vez
 
 let Materias = [CalculoDIV, CalculoDIVV, P1, GAL1, MD1, P2, Ec, F1, GAL2, PYE, MD2, LG, MN, P4, IIO, P3, AC, CV, ED, TL, FVC, SO, FO, OCA, FBD, RC, TP, IIS, IPF, PL, PIS, AA, AE, ICG, CGA, CG, FSI];
 const MateriasConOpcionales = [CalculoDIV, CalculoDIVV, P1, GAL1, MD1, P2, Ec, F1, GAL2, PYE, MD2, LG, MN, P4, IIO, P3, AC, CV, ED, TL, FVC, SO, FO, OCA, FBD, RC, TP, IIS, IPF, PL, PIS, AA, AE, ICG, CGA, CG, FSI];
@@ -88,17 +92,31 @@ function toggleOpcionales(){
 
 function toggleMateria(nombre){
 
-    if (!MateriasPersona.find( nombreAux => nombreAux == nombre )){
-        MateriasPersona.push(nombre);
-        actualizar();
-    }else{
-        MateriasPersona.splice(MateriasPersona.indexOf(nombre), 1);
-        actualizar();
+    let materiaActual = Materias.find( nombreAux => nombreAux.nombre == nombre );
+    switch (materiaActual.estado) {
+        case 0:
+            
+            break;
+        case 1:
+            MateriasPersona.push(materiaActual.curso);
+            break;
+        case 2:
+            MateriasPersona.push(materiaActual.nombre);
+            break;
+        case 3:
+            MateriasPersona.splice(MateriasPersona.indexOf(materiaActual.curso), 1);
+            MateriasPersona.splice(MateriasPersona.indexOf(materiaActual.nombre), 1);
+            break;
+        default:
+            break;
     }
+    actualizar();
 
 }
 
 function actualizar(){
+
+    creditos = 0;
 
     Materias.forEach( (materia) => {
 
@@ -107,34 +125,42 @@ function actualizar(){
         let estanTodas = true;
 
         materia.previas.forEach( (previa) => {
-            estanTodas = MateriasPersona.includes(previa.nombre) && estanTodas;
+            estanTodas = (MateriasPersona.includes(previa.nombre)||MateriasPersona.includes(previa)) && estanTodas;
         });
 
         if (estanTodas){
 
-            document.getElementById(materia.nombre).disabled = false;
-            document.getElementById(materia.nombre).style.background = "lightcoral";
+            
+            if (!MateriasPersona.find(elemento => elemento == materia.nombre) || !MateriasPersona.find( elemento => elemento == materia.curso )){
+                document.getElementById(materia.nombre).disabled = false;
+                document.getElementById(materia.nombre).style.background = "lightcoral";
+                materia.estado = 1;
+            }
+            if (MateriasPersona.find( elemento => elemento == materia.curso )){
+                document.getElementById(materia.nombre).style.background = "lightblue";
+                //creditos += materia.creditos;
+                materia.estado = 2;
+            }
+            if (MateriasPersona.find(elemento => elemento == materia.nombre)){
+                document.getElementById(materia.nombre).style.background = "lightgreen";
+                creditos += materia.creditos;
+                materia.estado = 3;
+            }      
 
         }else{
 
             if (MateriasPersona.includes(materia.nombre)){
                 MateriasPersona.splice(MateriasPersona.indexOf(materia.nombre), 1);
             }
+            if (MateriasPersona.includes(materia.curso)){
+                MateriasPersona.splice(MateriasPersona.indexOf(materia.curso), 1);
+            }
+            materia.estado = 0;
             document.getElementById(materia.nombre).disabled = true;
             document.getElementById(materia.nombre).style.background = "gray";
 
         }
 
-    });
-
-    creditos = 0;
-
-    MateriasPersona.forEach( (materia) => {
-        document.getElementById(materia).style.background = "lightgreen";
-        let buscarMateria = Materias.find( materiaAux => materiaAux.nombre == materia )
-        if (buscarMateria){
-            creditos += buscarMateria.creditos;
-        }
     });
 
     document.getElementById('titulo').textContent = `Materias | Créditos: ${creditos}`
